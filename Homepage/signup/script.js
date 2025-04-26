c89a905a4a004e6cdfe877b56e98e70b3625ffb4
Robinhood347 <165918354+Robinhood347@users.noreply.github.com>
2025-04-25 22:24:03 -0400

    Update script.js

--- Homepage/signup/script.js
+++ Homepage/signup/script.js
@@ -1,21 +1,26 @@
+document.getElementById("startGame").addEventListener("click", function() {
+  document.getElementById("homepage").style.display = "none"; 
+  document.getElementById("gameplay").style.display = "block"; 
+});
+
 document.getElementById('sign-up-form').addEventListener('submit', async (e) => {
   e.preventDefault();
-  
+
   const data = {
     username: document.getElementById('username').value,
     email: document.getElementById('email').value,
     password: document.getElementById('password').value
   };
 
+  const backendURL = "https://eldyn-io.onrender.com/signup";
+
   try {
-    const response = await fetch('/signup', {
+    const response = await fetch(backendURL, { 
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
     });
-    
+
     const result = await response.json();
     alert(result.message);
   } catch (error) {
